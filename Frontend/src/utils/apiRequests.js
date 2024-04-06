ad6909c727e14fa29a91adab91e922d6d381f0e9
Pradum Sonkar <pradumsonkar9112@gmail.com>
2024-04-06 17:17:40 +0530

    package

--- Frontend/src/utils/apiRequests.js
+++ Frontend/src/utils/apiRequests.js
@@ -1,4 +1,5 @@
-const host = "http://localhost:5000";
+// const host = "http://localhost:5000";
+const host = "https://quantum-it-task.onrender.com/";
 
 export const registerAPI = `${host}/api/user/register`;
 export const loginAPI = `${host}/api/user/login`;
